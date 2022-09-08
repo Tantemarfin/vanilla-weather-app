e26f6f1e3034a51554ffc430f3bd65367539b1d6
Tantemarfin <odessa812@gmx.de>
2022-09-08 23:04:58 +0200

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestemp) {
+  let date = new Date(timestemp);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  return days[day] + " " + hours + ":" + minutes;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temreparureElement = document.querySelector("#temperature");
@@ -5,11 +28,13 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let descriptionElement = document.querySelector("#description");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temreparureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "b129a7ea444c03743ffd270a55b06b87";
